99c12a317cb7476720b809531c77c539b3fd8eef
María J Castro Rivero <mj.castro.rivero@gmail.com>
2025-05-15 19:08:41 +0200

    Delete COSAS GENERALES/Documentacion/Curriculum/Curriculum directory

--- COSAS GENERALES/Documentacion/Curriculum/Curriculum/js/scripts.js
+++
@@ -1,6 +0,0 @@
-const container = document.querySelector('.container');
-const avatar = document.querySelector('.avatar');
-
-avatar.addEventListener('click', () => {
-  container.classList.toggle('expanded');
-});
